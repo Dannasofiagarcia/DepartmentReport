772a8aebaebee34bd55d50830822d20b2f2e0f65
Danna Garcia <dannasofiagarciatrujillo@hotmail.com>
2021-02-07 15:24:23 -0500

    Added labels

---
+++ ReporteMunicipiosPI/ReporteMunicipiosPI/Ui/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace ReporteMunicipiosPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar archivo de datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(38, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(604, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(137, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1204, 463);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Municipio",
+            "Isla",
+            "Área no municipada"});
+            this.comboBox1.Location = new System.Drawing.Point(977, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(434, 33);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(648, 616);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Generar gráfico";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1077, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Filtre los datos por tipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(390, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ruta del archivo de datos seleccionado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1466, 687);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ReporteMunicipiosPI/ReporteMunicipiosPI/Ui/Form1.cs
@@ -0,0 +1,87 @@
+﻿using ReporteMunicipiosPI.ui;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReporteMunicipiosPI
+{
+    public partial class Form1 : Form
+    {
+
+        private Model.DataManager dataManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataManager = new Model.DataManager();
+        }
+
+        //Button 1 cargar archivo de datos
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.ShowDialog();
+
+            textBox1.Text = openFileDialog1.FileName;
+
+            string path = textBox1.Text;
+
+            dataManager.createTable(path);
+
+            if (dataManager.Table != null)
+                dataGridView1.DataSource = dataManager.Table;
+        }
+
+        //Button 2 generar gráfico
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox1.Text != null)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                Form2 form2 = new Form2(textBox1.Text.ToString());
+                form2.ShowDialog();
+            }
+        }
+
+
+        //Combox de tipo para filtrar
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox1.Text != null)
+            {
+
+                if (comboBox1.SelectedItem.ToString().Equals("Municipio"))
+                {
+                    string region = "Municipio";
+                    string path = comboBox1.SelectedItem.ToString();
+
+                    dataManager.filterByType(textBox1.Text.ToString(), region);
+                }
+                else if (comboBox1.SelectedItem.ToString().Equals("Isla"))
+                {
+                    string region = "Isla";
+                    string path = comboBox1.SelectedItem.ToString();
+
+                    dataManager.filterByType(textBox1.Text.ToString(), region);
+
+                }
+                else if (comboBox1.SelectedItem.ToString().Equals("Área no municipada"))
+                {
+                    string region = "Isla";
+                    string path = comboBox1.SelectedItem.ToString();
+
+                    dataManager.filterByType(textBox1.Text.ToString(), region);
+
+                }
+            }
+        }
+    }
+}
+
+    
