facf51596f79cc87f58c94fcfcdda3f7bc28cf31
Julian A Rivera C <48961526+JulianAndresRiveraCarrillo@users.noreply.github.com>
2021-02-07 13:13:19 -0500

    Departmen class modified

--- ReporteMunicipiosPI/ReporteMunicipiosPI/model/Department.cs
+++ ReporteMunicipiosPI/ReporteMunicipiosPI/model/Department.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReporteMunicipiosPI.model
+namespace ReporteMunicipiosPI.Model
 {
     class Department
     {
+        private string name;
+        private int countMun;
+
+        public Department(string _name, int _countMun)
+        {
+            name = _name;
+            countMun = _countMun;
+        }
+
+        public string Name
+        {
+            get => name;
+        }
+
+        public string CountMun
+        {
+            get => countMun;
+            set => countMun = value;
+        }
     }
+
 }
