7d4c692dcef7628fc923d79ab9d7525a6e04406f
Danna Garcia <dannasofiagarciatrujillo@hotmail.com>
2021-02-07 12:32:07 -0500

    Created DataManager code and complete Form1

--- ReporteMunicipiosPI/ReporteMunicipiosPI/model/DataManager.cs
+++ ReporteMunicipiosPI/ReporteMunicipiosPI/model/DataManager.cs
@@ -8,5 +8,106 @@ namespace ReporteMunicipiosPI.model
 {
     class DataManager
     {
+
+        private DataTable table;
+
+        public DataTable Table
+        {
+            get => table;
+        }
+
+        public void refresh()
+        {
+            table.Clear();
+        }
+
+        public void filterByType(string filePath, string tipo)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            if (lines.Length > 1)
+            {
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+
+                    string[] data = Regex.Split(lines[i], ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                    if (data.Length > 0)
+                    {
+                        if (data[6].Equals(tipo))
+                        {
+                            DataRow row = table.NewRow();
+                            for (int j = 0; j < data.Length; j++)
+                            {
+                                try
+                                {
+                                    row[j] = data[j];
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("WARNING");
+                                }
+                            }
+
+                            table.Rows.Add(row);
+                        }
+                    }
+
+
+                }
+
+            }
+
+
+        }
+
+        public void createTable(string filePath)
+        {
+
+            table = new DataTable();
+
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                string firstLine = lines[0];
+
+                string[] labels = Regex.Split(firstLine, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                foreach (string header in labels)
+                {
+                    table.Columns.Add(new DataColumn(header));
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+
+
+                    string[] dataLine = Regex.Split(lines[i], ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                    DataRow row = table.NewRow();
+
+                    for (int j = 0; j < dataLine.Length; j++)
+                    {
+                        try
+                        {
+
+                            row[j] = dataLine[j];
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("WARNING: The CSV is poorly formatted!");
+                        }
+                    }
+
+                    table.Rows.Add(row);
+                }
+
+            }
+
+        }
+
     }
 }
--- ReporteMunicipiosPI/ReporteMunicipiosPI/ui/Form1.cs
+++ ReporteMunicipiosPI/ReporteMunicipiosPI/ui/Form1.cs
@@ -12,29 +12,44 @@ namespace ReporteMunicipiosPI
 {
     public partial class Form1 : Form
     {
+
+        private Model.DataManager dataManager;
+
         public Form1()
         {
             InitializeComponent();
+            dataManager = new Model.dataManager;
         }
 
         //Button 1 cargar archivo de datos
         private void button1_Click(object sender, EventArgs e)
         {
 
+            openFileDialog1.ShowDialog();
+
+            textBox1.Text = openFileDialog1.FileName;
+
+            string path = textBox1.Text;
+
+            dataManager.createTable(path);
+
+            if (dataManager.Table != null)
+                dataGridView1.DataSource = dataManager.Table;
         }
 
         //Button 2 generar gráfico
         private void button3_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text != "" && textBox1.Text != null)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                Form2 form2 = new Form2(textBox1.Text.ToString());
+                form2.ShowDialog();
+            }
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
 
-        }
-
-        //Combobox de tipo para filtrar
+        //Combox de tipo para filtrar
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
