96c3afcb13caf29cbf240dab11b4c51e66317707
Danna Garcia <dannasofiagarciatrujillo@hotmail.com>
2021-02-07 13:38:56 -0500

    Fixed deparment class

---
+++ ReporteMunicipiosPI/ReporteMunicipiosPI/Model/Department.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReporteMunicipiosPI.Model
+{
+    class Department
+    {
+        private string name;
+        private int countMun;
+
+        public Department(string _name, int _countMun)
+        {
+            name = _name;
+            countMun = _countMun;
+        }
+
+        public string Name
+        {
+            get => name;
+        }
+
+        public int CountMun
+        {
+            get => countMun;
+            set => countMun = value;
+        }
+    }
+
+}
